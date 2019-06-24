c267bec4b4042e915268cb472df13fe383a8af87
Andre Wiggins <andrewiggins@live.com>
2019-06-24 16:29:04 -0700

    Remove duplicated tests

--- compat/test/browser/index.test.js
+++ compat/test/browser/index.test.js
@@ -1,11 +1,9 @@
 import React, {
 	render,
-	cloneElement,
 	unmountComponentAtNode,
 	unstable_batchedUpdates
 } from '../../src';
-import { createElement as preactH } from 'preact';
-import { setupScratch, teardown, createEvent } from '../../../test/_util/helpers';
+import { setupScratch, teardown } from '../../../test/_util/helpers';
 
 describe('preact-compat', () => {
 
@@ -47,42 +45,5 @@ describe('preact-compat', () => {
 			unstable_batchedUpdates(spy, 'foo', 'bar');
 			expect(spy).to.be.calledWithExactly('foo');
 		});
-	});
-
-	it('should patch events', () => {
-		let spy = sinon.spy();
-		render(<div onClick={spy} />, scratch);
-		scratch.firstChild.click();
-
-		expect(spy).to.be.calledOnce;
-		expect(spy.args[0][0]).to.haveOwnProperty('persist');
-		expect(spy.args[0][0]).to.haveOwnProperty('nativeEvent');
-	});
-
-	it('should normalize ondoubleclick event', () => {
-		let vnode = <div onDoubleClick={() => null} />;
-		expect(vnode.props).to.haveOwnProperty('ondblclick');
-	});
-
-	it('should normalize onChange for textarea', () => {
-		let vnode = <textarea onChange={() => null} />;
-		expect(vnode.props).to.haveOwnProperty('oninput');
-		expect(vnode.props).to.not.haveOwnProperty('onchange');
-
-		vnode = <textarea oninput={() => null} onChange={() => null} />;
-		expect(vnode.props).to.haveOwnProperty('oninput');
-		expect(vnode.props).to.not.haveOwnProperty('onchange');
-	});
-
-	it('should normalize class+className even on components', () => {
-		function Foo(props) {
-			return <div class={props.class} className={props.className}>foo</div>;
-		}
-		render(<Foo class="foo" />, scratch);
-		expect(scratch.firstChild.className).to.equal('foo');
-		render(null, scratch);
-
-		render(<Foo className="foo" />, scratch);
-		expect(scratch.firstChild.className).to.equal('foo');
 	});
 });
