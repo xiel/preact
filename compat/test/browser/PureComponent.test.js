63a8b9218479427510188d28c426d4bac2f0fd9c
Andre Wiggins <andrewiggins@live.com>
2019-06-24 17:09:31 -0700

    Separate PureComponent tests

---
+++ compat/test/browser/PureComponent.test.js
@@ -0,0 +1,91 @@
+import React from '../../src';
+import { setupScratch, teardown } from '../../../test/_util/helpers';
+import { setupRerender } from 'preact/test-utils';
+
+describe('PureComponent', () => {
+
+	/** @type {HTMLDivElement} */
+	let scratch;
+
+	/** @type {() => void} */
+	let rerender;
+
+	beforeEach(() => {
+		scratch = setupScratch();
+		rerender = setupRerender();
+	});
+
+	afterEach(() => {
+		teardown(scratch);
+	});
+
+	it('should be a class', () => {
+		expect(React).to.have.property('PureComponent').that.is.a('function');
+	});
+
+	it('should pass props in constructor', () => {
+		let spy = sinon.spy();
+		class Foo extends React.PureComponent {
+			constructor(props) {
+				super(props);
+				spy(this.props, props);
+			}
+		}
+
+		React.render(<Foo foo="bar" />, scratch);
+
+		let expected = { foo: 'bar' };
+		expect(spy).to.be.calledWithMatch(expected, expected);
+	});
+
+	it('should only re-render when props or state change', () => {
+		class C extends React.PureComponent {
+			render() {
+				return <div />;
+			}
+		}
+		let spy = sinon.spy(C.prototype, 'render');
+
+		let inst = React.render(<C />, scratch);
+		expect(spy).to.have.been.calledOnce;
+		spy.resetHistory();
+
+		inst = React.render(<C />, scratch);
+		expect(spy).not.to.have.been.called;
+
+		let b = { foo: 'bar' };
+		inst = React.render(<C a="a" b={b} />, scratch);
+		expect(spy).to.have.been.calledOnce;
+		spy.resetHistory();
+
+		inst = React.render(<C a="a" b={b} />, scratch);
+		expect(spy).not.to.have.been.called;
+
+		inst.setState({ });
+		rerender();
+		expect(spy).not.to.have.been.called;
+
+		inst.setState({ a: 'a', b });
+		rerender();
+		expect(spy).to.have.been.calledOnce;
+		spy.resetHistory();
+
+		inst.setState({ a: 'a', b });
+		rerender();
+		expect(spy).not.to.have.been.called;
+	});
+
+	it('should update when props are removed', () => {
+		let spy = sinon.spy();
+		class App extends React.PureComponent {
+			render() {
+				spy();
+				return <div>foo</div>;
+			}
+		}
+
+		React.render(<App a="foo" />, scratch);
+		React.render(<App />, scratch);
+		expect(spy).to.be.calledTwice;
+	});
+});
--- compat/test/browser/component.test.js
+++ compat/test/browser/component.test.js
@@ -1,4 +1,3 @@
-import { setupRerender } from 'preact/test-utils';
 import { setupScratch, teardown } from '../../../test/_util/helpers';
 import React from '../../src';
 
@@ -7,12 +6,8 @@ describe('components', () => {
 	/** @type {HTMLDivElement} */
 	let scratch;
 
-	/** @type {() => void} */
-	let rerender;
-
 	beforeEach(() => {
 		scratch = setupScratch();
-		rerender = setupRerender();
 	});
 
 	afterEach(() => {
@@ -71,78 +66,6 @@ describe('components', () => {
 
 		expect(props).to.exist.and.deep.equal({
 			children: 'second'
-		});
-	});
-
-	describe('PureComponent', () => {
-		it('should be a class', () => {
-			expect(React).to.have.property('PureComponent').that.is.a('function');
-		});
-
-		it('should pass props in constructor', () => {
-			let spy = sinon.spy();
-			class Foo extends React.PureComponent {
-				constructor(props) {
-					super(props);
-					spy(this.props, props);
-				}
-			}
-
-			React.render(<Foo foo="bar" />, scratch);
-
-			let expected = { foo: 'bar' };
-			expect(spy).to.be.calledWithMatch(expected, expected);
-		});
-
-		it('should only re-render when props or state change', () => {
-			class C extends React.PureComponent {
-				render() {
-					return <div />;
-				}
-			}
-			let spy = sinon.spy(C.prototype, 'render');
-
-			let inst = React.render(<C />, scratch);
-			expect(spy).to.have.been.calledOnce;
-			spy.resetHistory();
-
-			inst = React.render(<C />, scratch);
-			expect(spy).not.to.have.been.called;
-
-			let b = { foo: 'bar' };
-			inst = React.render(<C a="a" b={b} />, scratch);
-			expect(spy).to.have.been.calledOnce;
-			spy.resetHistory();
-
-			inst = React.render(<C a="a" b={b} />, scratch);
-			expect(spy).not.to.have.been.called;
-
-			inst.setState({ });
-			rerender();
-			expect(spy).not.to.have.been.called;
-
-			inst.setState({ a: 'a', b });
-			rerender();
-			expect(spy).to.have.been.calledOnce;
-			spy.resetHistory();
-
-			inst.setState({ a: 'a', b });
-			rerender();
-			expect(spy).not.to.have.been.called;
-		});
-
-		it('should update when props are removed', () => {
-			let spy = sinon.spy();
-			class App extends React.PureComponent {
-				render() {
-					spy();
-					return <div>foo</div>;
-				}
-			}
-
-			React.render(<App a="foo" />, scratch);
-			React.render(<App />, scratch);
-			expect(spy).to.be.calledTwice;
 		});
 	});
 
